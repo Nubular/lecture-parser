a07b693f7ce632d61516eda41b721a9ac030fe2e
queenofthelosers <captainvhagar@gmail.com>
2021-01-25 18:31:25 +0530

    Revert to SynthesizeSpeech

--- mp3/main.go
+++ mp3/main.go
@@ -7,9 +7,7 @@ import(
     "github.com/aws/aws-sdk-go-v2/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/polly"
-    "github.com/aws/aws-sdk-go/service/s3"
-    "github.com/aws/aws-sdk-go/service/s3/s3manager"
-    "time"
+    "io"
     "io/ioutil"
 )
 
@@ -54,87 +52,50 @@ func updateConfig(audiofile string){
 
 func main(){
     
-    fname := "slide2_1" 
+    fname := "slide3_1" 
     s := "Please read this mp3" //Text to be converted to speech
-    checkfile := fname + ".mp3"
+    audiofile := fname + ".mp3"
     //Creates session based on credentials & config present in .aws directory 
     sess := session.Must(session.NewSessionWithOptions(session.Options{
         SharedConfigState: session.SharedConfigEnable,
     }))
     
     //Flag to check if mp3 has already been created
-    if mp3Exists(checkfile){
+    if mp3Exists(audiofile){
         fmt.Println("MP3 for this slide has already been generated.")
 
     } else {   
         //Making request to Polly
+        //Old code for 3000 characters - using synthesize speech
         svc := polly.New(sess)
-        input := &polly.StartSpeechSynthesisTaskInput{OutputFormat: aws.String("mp3"), OutputS3BucketName:aws.String("se-project-mp3-files") ,Text: aws.String(s), VoiceId: aws.String("Joanna")}
-        response,err := svc.StartSpeechSynthesisTask(input)
-        if err != nil{
-            fmt.Println("Here")
-            fmt.Println(err.Error());
-            os.Exit(1);
-        }
-        fmt.Println(response.SynthesisTask)
-        fmt.Printf("%T",response);
-        taskID := *response.SynthesisTask.TaskId +".mp3"
-        item :=  fname + ".mp3"
-        //Polly sends output to S3 bucket - filename in S3 bucket is same as taskID
-        time.Sleep(20*time.Second);
-        //Downloading from S3 -  - audio file named after the slide (fname)
-        downloader := s3manager.NewDownloader(sess);
-        file, err := os.Create(item)
-
+        input := &polly.SynthesizeSpeechInput{OutputFormat: aws.String("mp3"), Text: aws.String(s), VoiceId: aws.String("Joanna")}
+        output, err := svc.SynthesizeSpeech(input)
+    
         if err != nil {
-            fmt.Println("Error HERE")
-            fmt.Println(err);
-            os.Exit(1);
-        }
-        //Downloading the file onto system
-        defer file.Close()
-        numBytes, err := downloader.Download(file,
-            &s3.GetObjectInput{
-                Bucket: aws.String("se-project-mp3-files"),
-                Key:    aws.String(taskID),
-            })
-        if err != nil {
-            fmt.Println("This is the error");
-            fmt.Println(err);
-            os.Exit(1);
+            fmt.Println("Got error calling SynthesizeSpeech:")
+            fmt.Print(err.Error())
+            os.Exit(1)
         }
 
-        fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
-        updateConfig(item);
+        //create file
+        name := audiofile
+        outFile, err := os.Create(name)
+        if err != nil {
+            fmt.Println("Got error creating " + name + ":")
+            fmt.Print(err.Error())
+            os.Exit(1)
+        }
+        defer outFile.Close()
+        _, err = io.Copy(outFile, output.AudioStream)
+        if err != nil {
+            fmt.Println("Got error saving MP3:")
+            fmt.Print(err.Error())
+            os.Exit(1)
+        }
+        updateConfig(audiofile);
     }    
 }
     
     
     
-    //Old code for 3000 characters - using synthesize speech
-    // svc := polly.New(sess)
-    // input := &polly.SynthesizeSpeechInput{OutputFormat: aws.String("mp3"), Text: aws.String(s), VoiceId: aws.String("Joanna")}
-    // output, err := svc.SynthesizeSpeech(input)
-    
-    // if err != nil {
-    //     fmt.Println("Got error calling SynthesizeSpeech:")
-    //     fmt.Print(err.Error())
-    //     os.Exit(1)
-    // }
-
-    // //create file
-    // name := "random_audio.mp3"
-    // outFile, err := os.Create(name)
-    // if err != nil {
-    //     fmt.Println("Got error creating " + name + ":")
-    //     fmt.Print(err.Error())
-    //     os.Exit(1)
-    // }
-    // defer outFile.Close()
-    // _, err = io.Copy(outFile, output.AudioStream)
-    // if err != nil {
-    //     fmt.Println("Got error saving MP3:")
-    //     fmt.Print(err.Error())
-    //     os.Exit(1)
-    // }
-
+  
